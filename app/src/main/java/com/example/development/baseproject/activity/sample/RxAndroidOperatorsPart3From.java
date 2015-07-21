0240affbb17d7a5602e028211bdb3c7914165505
Shekar <shekar.shine@gmail.com>
2015-07-21 23:53:12 +0530

    Added some Rx operators

--- app/src/main/java/com/example/development/baseproject/activity/sample/RxAndroidOperatorsPart3From.java
+++ app/src/main/java/com/example/development/baseproject/activity/sample/RxAndroidOperatorsPart3From.java
@@ -8,6 +8,7 @@ import com.example.development.baseproject.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 
@@ -16,7 +17,7 @@ import rx.Observable;
  */
 public class RxAndroidOperatorsPart3From extends Activity {
     private TextView mTextView;
-    private ArrayList<Integer> number = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10));
+    private ArrayList<Integer> number = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +27,20 @@ public class RxAndroidOperatorsPart3From extends Activity {
         mTextView = (TextView) findViewById(R.id.rx_textview);
         Observable.from(number).flatMap(i -> Observable.just(square(i)))
                 .filter(number -> number != 25)
-                .take(5)
-                .subscribe(s -> System.out.println(s));
+                .take(15)
+                .repeat(5)
+                .distinct()
+                .distinctUntilChanged()
+                .sample(1, TimeUnit.SECONDS)// Take last element and emit it in 1 sec
+                .throttleFirst(1,TimeUnit.SECONDS)// Take first element and emit it in 1 sec
+                .subscribe(s -> System.out.println("======" + s));
         ;
     }
 
     private int square(Integer i) {
         return i * i;
+    }
+    public void setUI(Integer i) {
+        mTextView.setText(i.toString());
     }
 }
